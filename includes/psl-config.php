1ae10012418953729f6001ed451a533729297f80
Bhartendu Marodia <bharatmrd@gmail.com>
2016-04-17 09:26:11 +0530

    Added files via upload

---
+++ includes/psl-config.php
@@ -0,0 +1,13 @@
+<?php
+
+define("HOST", "localhost");
+define("USER", "secure_user");
+define("PASSWORD", "helloworld");
+define("DATABASE", "secure_login");
+
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+
+define("SECURE", FALSE);	// For development!! 
+	
+?>
